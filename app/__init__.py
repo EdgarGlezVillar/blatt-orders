717a14ae9a7e442cc95a885b7de05ba56500cd46
EdgarGlezVillar <edgar.gonzalezvr@udlap.mx>
2025-05-23 14:37:07 -0600

    Crear tablas desde app factory con app_context()

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,27 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .models import OrdenImpresion
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    # Corregir formato de URI para PostgreSQL
+    # Configuración de la base de datos
     uri = os.environ.get("DATABASE_URL")
     if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = uri
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    # Registrar Blueprint
+    # Registra tus blueprints aquí
     from .routes import main
     app.register_blueprint(main)
+
+    # Crea las tablas solo si no existen
+    with app.app_context():
+        db.create_all()
 
     return app
