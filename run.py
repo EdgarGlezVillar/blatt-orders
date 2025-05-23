1cb428fcd986a1c86578652845c9ff3b3094de7b
EdgarGlezVillar <edgar.gonzalezvr@udlap.mx>
2025-05-23 14:49:59 -0600

    create_all en producción para Render

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-from app import create_app
+from app import create_app, db
+from app.models import OrdenImpresion  # Asegúrate de importar el modelo
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+    print("✅ Tablas creadas (o ya existentes) en la base de datos PostgreSQL")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
