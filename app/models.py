3e11d06b8e38353df4f4aa7a7754148b186a0337
EdgarGlezVillar <edgar.gonzalezvr@udlap.mx>
2025-05-26 16:06:43 -0600

    Agregado modelo Cliente

--- app/models.py
+++ app/models.py
@@ -5,3 +5,8 @@ class Orden(db.Model):
     cliente = db.Column(db.String(100), nullable=False)
     producto = db.Column(db.String(100), nullable=False)
     cantidad = db.Column(db.Integer, nullable=False)
+
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    correo = db.Column(db.String(120), nullable=True)
