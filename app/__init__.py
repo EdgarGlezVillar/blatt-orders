a1cce2c0c310c4ce3d12eb79b3ae20553a575093
EdgarGlezVillar <edgar.gonzalezvr@udlap.mx>
2025-05-26 16:23:38 -0600

    Agregado db.create_all() para crear tablas en producción

--- app/__init__.py
+++ app/__init__.py
@@ -10,14 +10,17 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    # Usa tus propias credenciales de DATABASE_URL (de tu dashboard de PostgreSQL)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  # Lo cargas desde variables de entorno
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    # Importar rutas
+    # 👉 Importar rutas
     from .routes import main
     app.register_blueprint(main)
+
+    # 👉 Crear tablas si no existen
+    with app.app_context():
+        db.create_all()
 
     return app
