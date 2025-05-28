0d7a968c56cbfc770fa437c6e4880e5a688e3e79
EdgarGlezVillar <edgar.gonzalezvr@udlap.mx>
2025-05-28 13:52:47 -0600

    Agrega check_password al modelo Usuario

--- app/models.py
+++ app/models.py
@@ -38,9 +38,12 @@ class Usuario(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
-    rol = db.Column(db.String(20), nullable=False)  # 'admin' o 'general'
+    password_hash = db.Column(db.Text, nullable=False)  # Ya actualizado
+    rol = db.Column(db.String(20), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
