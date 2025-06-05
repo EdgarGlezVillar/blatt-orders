28b5e8f5e19bd2254614e695c08bdf490e4b96ef
EdgarGlezVillar <edgar.gonzalezvr@udlap.mx>
2025-06-05 12:51:20 -0600

    Cambios generales y usuario clase

--- app/models.py
+++ app/models.py
@@ -150,3 +150,18 @@ class OrdenDeCompra(db.Model):
 
     cotizacion = db.relationship('Cotizacion', backref=db.backref('ordenes', cascade='all, delete-orphan'))
     cliente = db.relationship('Cliente', backref=db.backref('ordenes', cascade='all, delete-orphan'))
+
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    rol = db.Column(db.String(20), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
